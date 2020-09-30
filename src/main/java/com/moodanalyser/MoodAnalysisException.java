3ef9f053675ce7aa9de166c11bc925980e29fe57
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-30 17:08:12 +0530

    [Ashutosh]Added:Empty Custom Exception

--- src/main/java/com/moodanalyser/MoodAnalysisException.java
+++ src/main/java/com/moodanalyser/MoodAnalysisException.java
@@ -2,9 +2,12 @@ package com.moodanalyser;
 
 public class MoodAnalysisException extends Exception {
 	
-	public MoodAnalysisException(String message)
-	{
-		super(message);
+	enum ExceptionType{
+		ENTERED_NULL,ENTERED_EMPTY
 	}
-
+	ExceptionType type;
+	public  MoodAnalysisException(ExceptionType type, String message) {
+		super(message);
+		this.type=type;
+	}
 }
